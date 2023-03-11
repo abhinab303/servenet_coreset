b09fe48379e8872822a5f65e0a80ce4b23853203
abhinab <abhinabacharya303@gmail.com>
2023-03-11 09:31:49 -0500

    run on rc now

--- hyper_param.py
+++ hyper_param.py
@@ -1,9 +1,9 @@
 ip_file_dir = "./data/"
 CLASS_NUM = category_num = 200
 max_len = 110
-BATCH_SIZE = 128
-LEARNING_RATE = 0.001
-epochs = 2
+BATCH_SIZE = 56
+LEARNING_RATE = 0.01
+epochs = 50
 
 
 def get_train_length():
@@ -14,5 +14,5 @@ def get_train_length():
 
 
 N = get_train_length()
-SUBSET_SIZE = 0.1
+SUBSET_SIZE = 1
 WORKERS = 1
